ff089e3e8973e0c61cb80f9712a4a3086d483e8b
ramkumar-m-7 <ramkumar.m@unifo.in>
2024-12-28 13:42:00 +0530

    port check

--- src/main/java/com/ramTech/ThriftWare/ThriftWareApplication.java
+++ src/main/java/com/ramTech/ThriftWare/ThriftWareApplication.java
@@ -1,13 +1,23 @@
 package com.ramTech.ThriftWare;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class ThriftWareApplication {
 
+	@Value("${PORT:10101}")
+    private String port;
 	public static void main(String[] args) {
 		SpringApplication.run(ThriftWareApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		System.out.println("Application is running on port: " + System.getenv("PORT"));
 	}
 
 }
